692944a0ea7f5108aef86a7aecd0051d4f4ab30e
Zsomi4 <zsomidev@gmail.com>
2022-03-09 10:31:47 +0100

    Rendszámok felvétele.

--- src/App.java
+++ src/App.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class App {
+    String[] rendszamTomb = {
+        "BDA-101",
+        "GZU-654",
+        "RTE-103",
+        "POI-542"
+    };
+    ArrayList<String> korozottLista = new ArrayList<>(Arrays.asList(rendszamTomb));
+
     public static void main(String[] args) throws Exception {
+        
     }
 }
